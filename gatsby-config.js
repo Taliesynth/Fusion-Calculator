008d5bc38f733803c9aecc9d2389d5822c92fbf7
Taliesynth <taliesynth@LAPTOP-UI3ET5LC>
2023-02-28 14:13:44 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Fusion Calculator`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
